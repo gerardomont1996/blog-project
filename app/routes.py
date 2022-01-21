9d7c35f9991fe249ec89dc53b3a09170489a1bd5
Jerry <gerardo.mont1996@gmail.com>
2022-01-21 14:36:07 -0500

    register and login

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from app import app 
+from flask import render_template
 
 
 
 
-@app.route('/')
+@app.route ('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
